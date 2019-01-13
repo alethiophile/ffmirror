c05db331ad49c8ba1b7e967b767ae09c09311f68
Tom Hunt <tomdicksonhunt@gmail.com>
2019-01-12 17:04:22 -0700

    Add request caching

--- ffmirror/util.py
+++ ffmirror/util.py
@@ -1,6 +1,6 @@
 # Utilities common to the fanfic site scrapers
 
-import time, urllib.request, urllib.error, re
+import time, urllib.request, urllib.error, re, hashlib, os, json, urllib.parse
 
 def fold_string_indiscriminately(s, n=80):
     """Folds a string (insert line-breaks where appropriate, to format
@@ -35,22 +35,76 @@ def fold_string_discriminately(s, n=80):
 
 def make_filename(title):
     title = title.lower().replace(" ", "_")
-    return re.sub("[^a-z0-9_]", "", title)
+    return re.sub("[^a-z0-9_.-]", "", title)
 
-def urlopen_retry(url, tries=3, delay=1):
+def url_hash(url):
+    return hashlib.sha256(url.encode()).hexdigest()
+
+class FakeRequest:
+    def __init__(self, d):
+        self.data = d
+
+    def read(self):
+        return self.data
+
+class NetworkFetcher:
+    def __init__(self, time_delay=2.0):
+        self.last_fetch = {}
+        self.delay = time_delay
+
+    def do_fetch(self, url, timeout=30, opener=None):
+        req = urllib.request.Request(url, headers={"User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:21.0) Gecko/20100101 Firefox/21.0"})  # noqa: E501
+        host = urllib.parse.urlsplit(url).netloc
+        if host not in self.last_fetch:
+            self.last_fetch[host] = 0
+        wait = time.time() - self.last_fetch[host]
+        if wait < self.delay:
+            time.sleep(self.delay - wait)
+        self.last_fetch[host] = time.time()
+
+        if opener is None:
+            open_func = urllib.request.urlopen
+        else:
+            open_func = opener.open
+
+        r = open_func(req, timeout=timeout)
+        if r.getcode() != 200:
+            raise Exception("non-200 return")
+
+        return r.read()
+
+def urlopen_retry(url, tries=3, delay=1, timeout=30, opener=None,
+                  cache_dir='/home/tom/.ffmirror_cache',
+                  fetcher=NetworkFetcher()):
     """Open a URL, with retries on failure. Spoofs user agent to look like Firefox,
     since FFnet 403s the urllib UA."""
-    req = urllib.request.Request(url, headers={"User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:21.0) Gecko/20100101 Firefox/21.0"})
+    fn = None
+    if cache_dir is not None:
+        uh = url_hash(url)
+        fn = os.path.join(cache_dir, uh)
+        if os.path.exists(fn) and os.stat(fn).st_mtime > time.time() - 43200:
+            try:
+                with open(fn) as inp:
+                    r = json.load(inp)
+                return FakeRequest(r['data'].encode())
+            except:
+                pass
     for i in range(tries):
         try:
-            r = urllib.request.urlopen(req)
+            # r = open_func(req, timeout=timeout)
+            data = fetcher.do_fetch(url, timeout, opener)
         except urllib.error.URLError as e:
             if i == tries - 1:
                 raise e
             time.sleep(delay)
         else:
-            return r
-        
+            if fn is not None:
+                o = { 'data': data.decode() }
+                with open(fn, 'w') as out:
+                    json.dump(o, out)
+                return FakeRequest(o['data'].encode())
+            return FakeRequest(data)
+
 def unsilly_import(name):
     # For dotted imports, this _imports_ the module you want, but _returns_ the
     # top-level package.
