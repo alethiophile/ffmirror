4fa73c1384fe5f0912e1b0af29fcbc91fa4e20f8
Tom Hunt <tomdicksonhunt@gmail.com>
2019-11-09 14:28:56 -0700

    Cleanup

--- ffmirror/__init__.py
+++ ffmirror/__init__.py
@@ -7,4 +7,5 @@ sites = { 'ffnet': ffmirror.ffnet.FFNet(),
           'fictionpress': ffmirror.ffnet.FictionPress() }
 
 urlres = [ (re.compile("https?://www.fanfiction.net/.*"), sites['ffnet']),
-           (re.compile("https?://www.fictionpress.com/.*"), sites['fictionpress']) ]
+           (re.compile("https?://www.fictionpress.com/.*"),
+            sites['fictionpress']) ]
